ed8cb1cc2890263a8295b28c12d56a84536d720a
harshil-jain-08 <harshil.jain@razorpay.com>
2022-08-29 18:01:59 +0530

    qq

--- src/Views/Items/ViewItems.js
+++ src/Views/Items/ViewItems.js
@@ -4,7 +4,7 @@ import Table from "../../components/Table";
 import { NavLink } from "react-router-dom";
 import { getItemsAPI } from "../../API";
 import { AppContext } from "../../context/AppContext";
-//comment
+//comment1234
 const ViewItems = () => {
     const { items, setItems } = useContext(AppContext);
     const getData = async () => {
