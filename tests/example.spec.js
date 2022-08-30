d90192d36e2e9f46342c810189b666a8f9be40a1
harshil-jain-08 <harshil.jain@razorpay.com>
2022-08-30 16:46:26 +0530

    try2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -18,7 +18,7 @@ test.describe("items test", () => {
     await itemsGetBeforeCreateNewItem(page);
     await page.goto(`${URLaddress}/items`);
 
-    await page.locator('[placeholder="Search"]').click();
+    await page.locator("text=dsfasd").isVisible();
     expect(await page.locator("tr").count()).toEqual(
       itemsBeforeCreateNewItem.length + 1
     );
